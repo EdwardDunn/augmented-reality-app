8c5bb2b761a061534e027845755b63be4bd12e7c
EdwardDunn <1505629@uad.ac.uk>
2018-12-13 14:39:32 +0000

    updated class description

--- app/src/main/java/edwarddunn/co/uk/augmented_reality_app/MainActivity.java
+++ app/src/main/java/edwarddunn/co/uk/augmented_reality_app/MainActivity.java
@@ -1,7 +1,10 @@
 package edwarddunn.co.uk.augmented_reality_app;
 
 /**
- * Created by edwar on 13/12/2018.
+ * Created by Edward Dunn
+ *
+ * Description: I have started this project to learn more about developing AR apps. This application
+ * will display a simple custom object on any flat surface. 
  */
 
 public class MainActivity {
